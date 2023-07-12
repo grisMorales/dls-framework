b30c409b64d70ce066921f46fe0f7f7121e4e2cc
Griselda Morales <griselda.juarez@frogdesign.com>
2023-07-11 20:37:03 -0600

    Initial Commit

---
+++ dls-framework/dls_framework.h
@@ -0,0 +1,18 @@
+//
+//  dls_framework.h
+//  dls-framework
+//
+//  Created by Griselda Morales on 11/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for dls_framework.
+FOUNDATION_EXPORT double dls_frameworkVersionNumber;
+
+//! Project version string for dls_framework.
+FOUNDATION_EXPORT const unsigned char dls_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dls_framework/PublicHeader.h>
+
+
